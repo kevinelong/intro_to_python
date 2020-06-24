d9c22c6bf8c5a6962fb5e4f0f7a2e8fe6af4f4f0
kevinelong <kevinelong@gmail.com>
2020-06-24 06:44:11 -0700

    round two

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+def numbers(v):
+    print(v, bin(v), hex(v), v)
+
+
 def show_ascii(name):
     for letter in name:
         ascii_code = ord(letter)
-        print(ascii_code, letter)
+        numbers(ascii_code)
 
 
 show_ascii("ABC")
+
+r = 200
+g = 240
+b = 64
+
+rgb = [r, g, b]
+
+for v in rgb:
+    numbers(v)
