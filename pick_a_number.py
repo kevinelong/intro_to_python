09b2bce59c23ce433e37e18419bb12b2d18d5a4f
kevinelong <kevinelong@gmail.com>
2020-06-25 08:43:26 -0700

    pick a number

--- pick_a_number.py
+++ pick_a_number.py
@@ -1,25 +1,45 @@
 # Selects an integer between 1 and 10
 from random import randint
 
-secret = randint(1, 10)
+playing = True
 
-print("I am thinking of a secret number between 1 and 10.")
-print("Guess and I will tell you if you are high, low, or just right.")
+while playing:
+    secret = randint(1, 10)
 
-guess: int = 0
+    print("I am thinking of a secret number between 1 and 10.")
+    print("Guess and I will tell you if you are high, low, or just right.")
 
-while guess != secret:
+    guess: int = 0
 
-    print("What is your guess?")
-    # Read a line of user input text from the Console into a String.
-    i = input()
+    while guess != secret:
 
-    if i:
-        guess = int(i)
+        # Read a line of user input text from the Console into a String.
+        i = input("What is your guess?")
 
-    if guess > secret:
-        print(f"Your guess {guess} is greater than secret.")
-    elif guess < secret:
-        print(f"Your guess {guess} is less than secret.")
+        if i:
+            guess = int(i)
 
-print(f"Perfect. You are right! {secret} is correct.")
+        if guess > secret:
+            print(f"Your guess {guess} is greater than secret.")
+        elif guess < secret:
+            print(f"Your guess {guess} is less than secret.")
+
+    print(f"Perfect. You are right! {secret} is correct.")
+
+    entry_incomplete = True
+    while entry_incomplete:
+        again = input("Press enter 'A' to play again. or enter 'Q' to quit")
+        again = again.upper()
+
+        if len(again) > 1:
+            print("Single letter only")
+        elif len(again) == 0:
+            print("Please enter a letter")
+        elif again not in ['A', 'Q']:
+            print("please enter Q or A")
+        else:
+            if again == "A":
+                playing = True
+            elif again == "Q":
+                playing = False
+            entry_incomplete = False
