549bae9149b1da1c3f0c4b836be4288c8c9eff06
kevinelong <kevinelong@gmail.com>
2021-01-27 15:59:57 -0800

    add missing colon

--- dice.py
+++ dice.py
@@ -6,7 +6,7 @@ from random import randint
 #     print(randint(1,6), end=" ")
 # print("")
 
-def roll(quantity=5, sides=6)
+def roll(quantity=5, sides=6):
     total = 0
     # TODO loop through range up to quantitiy,
     #  and append each value to the total
