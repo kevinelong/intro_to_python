0872375cbd7ae55e2624871ecc7628f373de3604
kevinelong <kevinelong@gmail.com>
2021-08-24 08:34:48 -0700

    fix format error and add commas

--- fstrings.py
+++ fstrings.py
@@ -2,5 +2,5 @@ quantity = 123
 product = "book"
 price = 99.99
 total = quantity * price
-output = f"{quantity} time {price} for {product} totals: { total.2f }."
+output = f"{quantity} time {price} for {product}, {total:,.2f} total."
 print(output)
